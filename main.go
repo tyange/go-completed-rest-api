8fd0d29d4a653f6c583e07a6df2c4d6cc98345c0
tyange <usun16@gmail.com>
2024-01-14 19:53:44 +0900

    add get all events

--- main.go
+++ main.go
@@ -19,7 +19,10 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -35,7 +38,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
